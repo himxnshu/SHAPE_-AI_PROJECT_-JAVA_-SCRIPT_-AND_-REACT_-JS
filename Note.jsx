b2ce3a51d97d5feba73c9c03358f7ddccc625354
himxnshu <84908841+himxnshu@users.noreply.github.com>
2021-06-06 15:48:47 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Note(){
+ return (
+ <div className="note">
+<h1> My Hobbies </h1>
+<p>
+I love to read. Reading is my hobby. I read books belonging to all genres. But it is adventure and mystery stories that keep me the most engaged. I love to read the Harry Potter series of books by JK Rowling. I also liked reading books by Enid Blyton.
+   </p>
+ </div>  
+  );
+}
+
+export default Note;
